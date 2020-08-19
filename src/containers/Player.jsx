c108fee465e1ddfd7849ac2b92fc22a17b2ea44f
Angie Garcia <Angiegarciaoficial@gmail.com>
2020-08-18 19:43:39 -0500

    Terminando de detallar nuestro player,(Destructuring)

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
+import { connect } from 'react-redux';
+import { getVideoSource } from '../actions';
 import '../assets/styles/components/Player.scss';
+import NotFound from './NotFound';
 
 const Player = (props) => {
-  const { id } = props.match.params;
-  return (
+  {}
+  // const { id } = props.match.params;
+  const { playing, match: { params: { id } } } = props;
+  const hasPlaying = Object.keys(playing).length > 0;
+
+  useLayoutEffect(() => {
+    props.getVideoSource(id);
+  }, []);
+
+  return hasPlaying ? (
     <div className='Player'>
       <video controls autoPlay>
-        <source src='' type='video/mp4' />
+        <source src={playing.source} type='video/mp4' />
       </video>
       <div className='Player-back'>
         <button type='button' onClick={() => props.history.goBack()}>
           Regresar
         </button>
       </div>
-
     </div>
-  );
+  ) : <NotFound />;
 };
 
-export default Player;
+const mapStateToProps = (state) => {
+  return {
+    playing: state.playing,
+  };
+};
+const mapDispatchToProps = {
+  getVideoSource,
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Player);
